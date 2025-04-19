4ef0878775a7407b791f8fd75946a2c71280e6a5
Akila Madhubhashitha <akilamadhubhashith@gmail.com>
2025-04-20 00:09:30 +0530

    change

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -25,7 +25,7 @@ export default function AdminPage() {
     }
     
     setLoading(true);
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/me`, {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
